05f3e658339987b20bcc850c3394b3bc0a611c6a
Amirhosein Barati <amirhoseinbarati75@gmail.com>
2019-05-09 19:10:40 +0430

    Initial commit

---
+++ dimener/src/main/java/ir/am3n/dimener/Dimener.java
@@ -0,0 +1,62 @@
+package ir.am3n.dimener;
+
+import android.content.Context;
+
+public class Dimener {
+
+
+    public static int getTxtTextsizeXSmallS(Context context) {
+        return context.getResources().getDimensionPixelSize(R.dimen.txt_textsize_xsmall_s);
+    }
+    public static int getTxtTextsizeXSmallM(Context context) {
+        return context.getResources().getDimensionPixelSize(R.dimen.txt_textsize_xsmall_m);
+    }
+    public static int getTxtTextsizeXSmallL(Context context) {
+        return context.getResources().getDimensionPixelSize(R.dimen.txt_textsize_xsmall_l);
+    }
+
+
+    public static int getTxtTextsizeSmallS(Context context) {
+        return context.getResources().getDimensionPixelSize(R.dimen.txt_textsize_small_s);
+    }
+    public static int getTxtTextsizeSmallM(Context context) {
+        return context.getResources().getDimensionPixelSize(R.dimen.txt_textsize_small_m);
+    }
+    public static int getTxtTextsizeSmallL(Context context) {
+        return context.getResources().getDimensionPixelSize(R.dimen.txt_textsize_small_l);
+    }
+
+
+    public static int getTxtTextsizeMediumS(Context context) {
+        return context.getResources().getDimensionPixelSize(R.dimen.txt_textsize_medium_s);
+    }
+    public static int getTxtTextsizeMediumM(Context context) {
+        return context.getResources().getDimensionPixelSize(R.dimen.txt_textsize_medium_m);
+    }
+    public static int getTxtTextsizeMediumL(Context context) {
+        return context.getResources().getDimensionPixelSize(R.dimen.txt_textsize_medium_l);
+    }
+
+
+    public static int getTxtTextsizeLargeS(Context context) {
+        return context.getResources().getDimensionPixelSize(R.dimen.txt_textsize_large_s);
+    }
+    public static int getTxtTextsizeLargeM(Context context) {
+        return context.getResources().getDimensionPixelSize(R.dimen.txt_textsize_large_m);
+    }
+    public static int getTxtTextsizeLargeL(Context context) {
+        return context.getResources().getDimensionPixelSize(R.dimen.txt_textsize_large_l);
+    }
+
+
+    public static int getTxtTextsizeXLargeS(Context context) {
+        return context.getResources().getDimensionPixelSize(R.dimen.txt_textsize_xlarge_s);
+    }
+    public static int getTxtTextsizeXLargeM(Context context) {
+        return context.getResources().getDimensionPixelSize(R.dimen.txt_textsize_xlarge_m);
+    }
+    public static int getTxtTextsizeXLargeL(Context context) {
+        return context.getResources().getDimensionPixelSize(R.dimen.txt_textsize_xlarge_l);
+    }
+
+}
